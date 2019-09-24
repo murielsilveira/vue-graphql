07e6180ac4f1d38145969f7dc94560a02dd7741d
Muriel Silveira <murielkong@gmail.com>
2019-09-25 00:47:50 +0200

    feat: configure apollo and add a simple query from a random graphql api

--- src/main.js
+++ src/main.js
@@ -1,11 +1,22 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import "./registerServiceWorker";
+import ApolloClient from 'apollo-boost'
+import Vue from 'vue'
+import VueApollo from 'vue-apollo'
+import App from './App.vue'
+import router from './router'
+import './registerServiceWorker'
 
-Vue.config.productionTip = false;
+const apolloClient = new ApolloClient({
+  uri: 'https://graphql-pokemon.now.sh',
+})
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+})
+
+Vue.use(VueApollo)
+Vue.config.productionTip = false
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount("#app");
+  apolloProvider,
+  render: h => h(App),
+}).$mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,24 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
+    <div v-if="pokemon">{{ pokemon.name }} is comming from a graphql api</div>
   </div>
 </template>
 
 <script>
+import gql from 'graphql-tag'
 
 export default {
-  name: "home",
-};
+  name: 'home',
+  apollo: {
+    pokemon: gql`
+      query {
+        pokemon(name: "Pikachu") {
+          id
+          name
+        }
+      }
+    `,
+  },
+}
 </script>
